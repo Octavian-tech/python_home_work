ce6870a4824f13da13f53f39fddeeaff7b7e1a23
Octavian-tech <munteanu.octavian.09@gmail.com>
2023-08-08 17:39:06 +0300

    Some changes

--- game.py
+++ game.py
@@ -1,20 +1,3 @@
-table = {
-    1: '-',
-    2: '-',
-    3: '-',
-    4: '-',
-    5: '-',
-    6: '-',
-    7: '-',
-    8: '-',
-    9: '-',
-}
-
-jucatorulCurent = "X"
-castigator = None
-joculRuleaza = True
-
-
 def printTable(table):
     print(table[1] + " | " + table[2] + " | " + table[3])
     print("---------")
@@ -23,125 +6,107 @@ def printTable(table):
     print(table[7] + " | " + table[8] + " | " + table[9])
 
 
-def playerInput(table):
-    select = int(input("Selecteaza optiunea 1-9:"))
-    if table[select] == '-':
-        table[select] = jucatorulCurent
-    else:
-        print("Oops jucatorul a ales deja acea pozitie.")
-
-
-def checkHorizontal(table):
-    global castigator
-    if table[1] == table[2] == table[3] != "-":
-        castigator = table[1]
-        return True
-    elif table[4] == table[5] == table[6] != "-":
-        castigator = table[4]
-        return True
-    elif table[7] == table[8] == table[9] != "-":
-        castigator = table[7]
-        return True
-
-
-def checkRow(table):
-    global castigator
-    if table[1] == table[4] == table[7] != "-":
-        castigator = table[1]
-        return True
-    elif table[2] == table[5] == table[8] != "-":
-        castigator = table[2]
-        return True
-    elif table[3] == table[6] == table[9] != "-":
-        castigator = table[3]
-        return True
-
-
-def checkDiag(table):
-    global castigator
-    if table[1] == table[5] == table[9] != "-":
-        castigator = table[1]
-        return True
-    elif table[3] == table[5] == table[7] != "-":
-        castigator = table[2]
-        return True
-
-
-choose_symbol = input("ALEGE SIMBOLUL:")
-choose_pc = 'X'
-choose_position = None
-
-if choose_symbol == 'X':
-    choose_pc = '0'
-
-
-def test_move(lista, symbol, table):
-    next_move = None
-
-    for i in lista:
-        if table[int(i[0])] == table[int(i[1])] == symbol and i[2] == '-':
-            next_move = i[2]
-        elif table[int(i[0])] == table[int(i[2])] == symbol and i[1] == '-':
-            next_move = i[1]
-        elif table[int(i[1])] == table[int(i[2])] == symbol and i[0] == '-':
-            next_move = i[0]
-
-        if next_move is not None:
-            print(next_move)
-            return next_move
-
+def set_next_move(lista, symbol, table):
+    for item in lista:
+        if table[item[0]] == table[item[1]] == symbol and table[item[2]] == '-':
+            return item[2]
+        elif table[item[0]] == table[item[2]] == symbol and table[item[1]] == '-':
+            return item[1]
+        elif table[item[1]] == table[item[2]] == symbol and table[item[0]] == '-':
+            return item[0]
     return None
 
 
-def pc_choose(table, list, symbol, symbol2):
-    if test_move(list, symbol, table) is not None:
-        table[test_move(list, symbol, table)] = symbol
-        printTable(table)
-    elif test_move(list, symbol2, table) is not None:
-        table[test_move(list, symbol2, table)] = symbol2
-        printTable(table)
-    elif table[5] == '-':
-        table[5] = symbol
-    elif table[1] == '-':
-        table[1] = symbol
-    elif table[3] == '-':
-        table[3] = symbol
-    elif table[7] == '-':
-        table[7] = symbol
-    elif table[9] == '-':
-        table[9] = symbol
-
-    return table
+def is_victory(table, icon):
+    if (table[1] == table[2] == table[3] == icon) or \
+            (table[4] == table[5] == table[6] == icon) or \
+            (table[7] == table[8] == table[9] == icon) or \
+            (table[1] == table[4] == table[7] == icon) or \
+            (table[2] == table[5] == table[8] == icon) or \
+            (table[3] == table[6] == table[9] == icon) or \
+            (table[1] == table[5] == table[9] == icon) or \
+            (table[3] == table[5] == table[7] == icon):
+        return True
+    else:
+        return False
 
 
-def control_position(table, position) -> int:
+def machine_player(table, lista, symbol, symbol2):
+    # Verifica daca poate castiga
+    next_move = set_next_move(lista, symbol, table)
+    if next_move is not None:
+        return next_move
+
+    # Verifica daca juctorul uman poate castiga si blocheaza mutarea
+    next_move = set_next_move(lista, symbol2, table)
+    if next_move is not None:
+        return next_move
+
+    if table[5] == '-':
+        return 5
+
+    if table[1] == '-':
+        return 1
+    if table[3] == '-':
+        return 3
+    if table[7] == '-':
+        return 7
+    if table[9] == '-':
+        return 9
+
+    # Alege orice alta mutare disponibila
+    for pos in range(1, 10):
+        if table[pos] == '-':
+            return pos
+
+
+def check_position(table, position) -> int:
     while table[int(position)] != '-':
+        print("Pozitia este ocupata alege din nou \n")
         position = input("Alege pozitia:")
     return int(position)
 
 
-lista = [[1, 2, 3],
-         [4, 5, 6],
-         [7, 8, 9],
-         [1, 4, 7],
-         [2, 5, 8],
-         [3, 6, 9],
-         [1, 5, 9],
-         [3, 5, 7]]
+list_pos_castig = [[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9],
+                   [1, 4, 7],
+                   [2, 5, 8],
+                   [3, 6, 9],
+                   [1, 5, 9],
+                   [3, 5, 7]]
 
-if choose_symbol == 'X':
+your_symbol = input(("Alege simbolul tau (X sau 0):"))
+pc_symbol = 'X'
+your_position = None
+
+if your_symbol == 'X':
+    pc_symbol = '0'
+
+table = {pos: '-' for pos in range(1, 10)}
+print(table)
+
+while "-" in table.values():
+    your_position = input("Alege pozitia: ")
+
+    table[check_position(table, position=your_position)] = your_symbol
+
     printTable(table)
-    while "-" in table.values():
-        choose_position = input("Alege pozitia:")
-        table[control_position(table, position=choose_position)] = 'X'
-        print(table)
-        printTable(table)
-        table = pc_choose(table, lista, 'O', 'X')
+    print('#####################')
 
-        printTable(table)
+    if is_victory(table, your_symbol):
+        print("Ai castigat")
+        break
 
-print(choose_symbol)
-# test_move(list, choose_symbol)
+    if "-" not in table.values():
+        print("Jocul sa finalizat")
+        break
 
-# while joculRuleaza:
-#     printTable(table)
+    pc_position = machine_player(table, lista=list_pos_castig, symbol=pc_symbol, symbol2=your_symbol)
+    print(pc_position)
+    table[pc_position] = pc_symbol
+    printTable(table)
+
+    if is_victory(table, pc_symbol):
+        print("0 wins! Congratulation")
+        break
